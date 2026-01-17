13bec050d756630d15a67314c74c5db2c9aaca67
AlinaPathan <alinapathan2004@gmail.com>
2026-01-17 17:34:50 +0530

    tasklist divided according to status

---
+++ src/components/TaskList/AcceptTask.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AcceptTask = () => {
+  return (
+  <div className='bg-yellow-500 rounded-xl w-[300px] h-full flex-shrink-0 p-5'>
+   <div className='flex justify-between items-center text-white'>
+    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+    <h4>20 feb 2024</h4>
+   </div>
+   <h2 className='mt-5 text-2xl font-semibold text-white'>New Task id to make video.</h2>
+
+<p className='text-sm text-white mt-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, molestias?</p>
+
+   </div>    
+)
+}
+
+export default AcceptTask
