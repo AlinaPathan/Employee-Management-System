21431cca0a695ff00904e1614824d4f3c2ba55be
AlinaPathan <alinapathan2004@gmail.com>
2026-01-29 13:35:21 +0530

    error fixed

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext ,useState ,useEffect } from 'react'
-import { getLocalStorage ,setLocalStorage } from '../utils/localStorage'
+import { getLocalStorage ,setLocalStorage } from '../utils/LocalStorage.jsx'
 
 export const AuthContext=createContext()//global context any componant can access the datawhat useeffect does explain
 
